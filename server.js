7859b7fa411ca7f422690f2bd947ff548e162272
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-05-06 13:25:40 +0000

    starting out

--- server.js
+++ server.js
@@ -10,12 +10,21 @@ const {Client} = require('mongodb')
 const app = express();
 app.use(cors());
 const port = 3000;
-const APIKey = ""
+const APIKey = process.env.APIKey
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', serverHandler);
+//app.get('/getData',getDataHandler)
+
+// function getDataHandler(req,res){
+//     let url = `https://api.elsevier.com/analytics/scival/country/metrics?metricTypes=ScholarlyOutput&countryIds=400&yearRange=10yrs&includeSelfCitations=true&byYear=true&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=false&${APIKey}`
+//     axios.get(url)
+//     .then(response => {
+//         res.json(response)
+//     })
+// }
 
 function serverHandler(req, res) {
     res.send("hello world");
